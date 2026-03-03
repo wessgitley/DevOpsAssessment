9dc029274bb4550fec275ab647fb012d3070c628
wessgitley <wesmut00@gmail.com>
2026-03-03 12:15:52 +0300

    Initial commit

---
+++ src/Welcome.java
@@ -0,0 +1,23 @@
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+
+public class Welcome {
+
+  JFrame frame = new JFrame();
+  JLabel welcomeLabel = new JLabel("Hello");
+
+  Welcome(String userID) {
+    welcomeLabel.setText("Welcome " + userID);
+    welcomeLabel.setBounds(0, 0, 200, 35);
+    welcomeLabel.setFont(new Font(null, Font.PLAIN, 25));
+    frame.add(welcomeLabel);
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.setSize(420, 420);
+    frame.setLayout(null);
+    frame.setVisible(true);
+
+  }
+
+}
